c5a214b3cbb0e0a8b6f023dad677e8caf971637c
Thomas Granbohm <thomas.granbohm@gmail.com>
2022-03-29 17:23:24 +0200

    Fixed basePath error

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: false,
-	basePath: process.env.BASE_PATH || "/",
+	basePath: process.env.BASE_PATH || "",
 };
 
 module.exports = nextConfig;
