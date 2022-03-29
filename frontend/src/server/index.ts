d09f039bab2519e3a9e394e207d87894f16e80d9
Thomas Granbohm <thomas.granbohm@gmail.com>
2022-03-29 17:48:10 +0200

    Added custom server

---
+++ frontend/src/server/index.ts
@@ -0,0 +1,29 @@
+import { createServer } from "http";
+import next from "next";
+import { parse } from "url";
+
+const dev = process.env.NODE_ENV !== "production";
+const hostname = "localhost";
+const port = 3000;
+
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+	createServer(async (req, res) => {
+		try {
+			const parsedUrl = parse(req.url || "", true);
+			const { pathname } = parsedUrl;
+
+			if (!pathname?.startsWith("/v1")) {
+				await handle(req, res, parsedUrl);
+			}
+		} catch (err) {
+			console.error("Error occurred handling", req.url, err);
+			res.statusCode = 500;
+			res.end("internal server error");
+		}
+	}).listen(port, () => {
+		console.log(`> Ready on http://${hostname}:${port}`);
+	});
+});
