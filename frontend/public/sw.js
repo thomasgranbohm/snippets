f4400b30d38c460aec2e23e240bf31197ba1014f
Thomas Granbohm <thomas.granbohm@gmail.com>
2022-04-11 14:08:58 +0200

    Added ServiceWorker

---
+++ frontend/public/sw.js
@@ -0,0 +1,47 @@
+const CACHES = [
+	{
+		name: "snippets-images",
+		regex: /\/snippets\/([0-9a-zA-Z\-]+)\/image$/,
+	},
+];
+
+self.addEventListener("install", (event) => {
+	event.waitUntil(Promise.all(CACHES.map(({ name }) => caches.open(name))));
+
+	self.skipWaiting();
+});
+
+self.addEventListener("fetch", (event) => {
+	event.respondWith(
+		caches
+			.match(event.request)
+			.then((resp) => {
+				if (resp) return resp;
+
+				return fetch(event.request).then((response) => {
+					if (
+						!response ||
+						response.status !== 200 ||
+						response.type !== "basic"
+					) {
+						return response;
+					}
+
+					for (const { name, regex } of CACHES) {
+						if (regex.test(response.url)) {
+							const responseToCache = response.clone();
+
+							caches
+								.open(name)
+								.then((cache) =>
+									cache.put(event.request, responseToCache)
+								);
+						}
+					}
+
+					return response;
+				});
+			})
+			.catch((err) => console.error(err))
+	);
+});
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,8 +1,16 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { useEffect } from "react";
 
 function CustomApp({ Component, pageProps }: AppProps) {
+	useEffect(() => {
+		console.log("serviceWorker" in navigator);
+		if ("serviceWorker" in navigator) {
+			navigator.serviceWorker.register("/sw.js");
+		}
+	}, []);
+
 	return (
 		<>
 			<Head>
